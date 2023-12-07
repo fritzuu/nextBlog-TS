f96b6531963ca095afa51414c6dbead666644f02
Revan Alifian Zhafran <igtrvnaz@gmail.com>
2023-12-07 17:12:08 +0700

    first commit

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,26 @@
+import { AuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import NextAuth from "next-auth/next";
+
+const authOptions: AuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: "Iv1.48955eaf95f32593",
+      clientSecret: "6f1045430e01320367a125976528222f8bfbd5c7",
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }: any) {
+      console.log(session, token);
+
+      session.user.name = `${session?.user?.name}_${token?.sub}`;
+
+      return session;
+    },
+  },
+  secret: "default_secret_key",
+};
+
+const nextAuth = NextAuth(authOptions);
+
+export { nextAuth as GET, nextAuth as POST };
